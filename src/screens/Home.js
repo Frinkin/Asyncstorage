0e7d937b4ad575948f493dc464df41142521012b
Frinkin <tatdattamtre@gmail.com>
2022-10-01 13:31:33 +0700

    commitment

---
+++ src/screens/Home.js
@@ -0,0 +1,10 @@
+import { Alert, StyleSheet, Text, View } from "react-native";
+
+export default function Home({ navigation }) {
+    
+    return (
+      <View >
+        <Text>This is home.</Text>
+      </View>
+    );
+  }
